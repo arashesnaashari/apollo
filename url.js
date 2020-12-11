cefafb0f62012dac7b96856c7b820193823698ac
arash <arash.asnaashari@gmail.com>
2020-12-11 12:01:34 +0330

    vercel

--- url.js
+++ url.js
@@ -1,5 +1,5 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://bookgrccccam-jtviq0oxm.vercel.app"
+    ? "https://bookgram-jtviq0oxm.vercel.app"
     : "http://localhost:3000";
 export default baseUrl;
