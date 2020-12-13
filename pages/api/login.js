89b56471431a338d3f9ec3d4abdd11b05bcba9ff
arash <arash.asnaashari@gmail.com>
2020-12-13 16:56:52 +0330

    10.3.5

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,32 +4,36 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 dbConnect();
 export default async function handler(req, res) {
-  try {
-    res.setHeader("Allow", "POST");
-    const Ouser = await User.findOne({ username: req.body.username });
-    if (!Ouser) {
-      res.status(400).json({ msg: "we havent you" });
-    }
-    const isEqual = await bcrypt.compare(req.body.password, Ouser.password);
-    if (!isEqual) {
-      res.status(400).json({ msg: "password wrong" });
-    }
-
-    // const token = jwt.sign(
-    //   { userId: Ouser.id, username: Ouser.username },
-    //   "somSuperKey",
-    //   {
-    //     expiresIn: "1h",
-    //   }
-    // );
-    // const data = {
-    //   userId: Ouser.id,
-    //   token: token,
-    //   tokenExpire: 1,
-    // };
-
-    res.status(201).json({ msg: Ouser });
-  } catch (error) {
-    res.status(400).json({ msg: error });
+ 
+  switch (req.method) {
+    case 'POST':
+      const Ouser = await User.findOne({ username: req.body.username });
+      if (!Ouser) {
+        res.status(400).json({ msg: "we havent you" });
+      }
+      const isEqual = await bcrypt.compare(req.body.password, Ouser.password);
+      if (!isEqual) {
+        res.status(400).json({ msg: "password wrong" });
+      }
+  
+      const token = jwt.sign(
+        { userId: Ouser.id, username: Ouser.username },
+        "somSuperKey",
+        {
+          expiresIn: "1h",
+        }
+      );
+      const data = {
+        userId: Ouser.id,
+        token: token,
+        tokenExpire: 1,
+      };
+  
+      res.status(201).json({ msg: Ouser });
+      break;
+  
+    default:
+      res.setHeader('Allow', ['GET', 'PUT'])
+      res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
