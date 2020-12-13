a081f9ca06ca30b57323cf9aaf43165a34670e1e
arash <arash.asnaashari@gmail.com>
2020-12-13 17:31:33 +0330

    10.3.8

--- components/layout/Form.js
+++ components/layout/Form.js
@@ -51,7 +51,7 @@ function Form() {
         }`,
       };
       try {
-        const res = await fetch("/api/graphql", {
+        const res = await fetch("https://bookgram.vercel.app/api/graphql", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(query),
