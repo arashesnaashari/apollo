15a2686041d85becefa90b41e914f4b12a74f8b3
arash <arash.asnaashari@gmail.com>
2021-01-11 08:26:28 +0330

    73.21

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,48 +1,65 @@
 import Layout from "../../components/layout/Layout";
 import { useContext } from "react";
 import PostContainer from "../../components/Post/postsContainer";
-import queryGraphQl from "../../shared/query-graphql/index";
+// import queryGraphQl from "../../shared/query-graphql/index";
 import BooksContext from "../../context/books-context";
 
 export default function propssing(props) {
   const context = useContext(BooksContext);
   context.books = props.dataSearch.books;
+  fetch("/api/graphql", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
+    query {
+      books(limit:0) {
+        title
+        author
+        _id
+        
+      }
+    }
+    `,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
   return (
     <>
       <Layout navbar={props.dataSearch.books}>
-        <PostContainer data={props.data.posts}></PostContainer>
+        <h1>j</h1>
       </Layout>
     </>
   );
 }
 
-export async function getServerSideProps() {
-  const dataQQ = await queryGraphQl(`query {
-      posts(limit:0) {
-        _id
-    title
-    body
-    date
-    image
-    creator {
-      username
-      profileURL
-      _id
-    }
-      }
-    
-  }`);
+// export async function getServerSideProps() {
+//   const dataQQ = await queryGraphQl(`query {
+//       posts(limit:0) {
+//         _id
+//     title
+//     body
+//     date
+//     image
+//     creator {
+//       username
+//       profileURL
+//       _id
+//     }
+//       }
 
-  const dataSearch = await queryGraphQl(`query {
-    books(limit:0) {
-      title
-    image
-    _id
-    author
-    }
-  
-}`);
-  return {
-    props: { data: dataQQ, dataSearch: dataSearch },
-  };
-}
+//   }`);
+
+//   const dataSearch = await queryGraphQl(`query {
+//     books(limit:0) {
+//       title
+//     image
+//     _id
+//     author
+//     }
+
+// }`);
+//   return {
+//     props: { data: dataQQ, dataSearch: dataSearch },
+//   };
+// }
